f12394da3c9400d6e878abf102308820711e72c1
Julian Gruber <julian@juliangruber.com>
2013-01-15 11:24:19 +0100

    sites can be a function now

---
+++ test.js
@@ -0,0 +1,14 @@
+require('./')()
+  .title('My Review')
+  .sites(function (cb) {
+    setTimeout(function () {
+      Math.random() > 0.5
+        ? cb(null, { google : 'http://google.com/', github : 'https://github.com/' })
+        : cb(null, { apple : 'http://apple.com/', yahoo : 'http://yahoo.com/' })
+    }, 10)
+  })
+  .resolutions(['1900x1600', '1280x1024', '800x600'])
+  .wait(0)
+  .listen(5000, function () {
+    console.log('-> Review on port 5000')
+  })
